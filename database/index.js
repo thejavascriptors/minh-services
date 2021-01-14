171e511ab94e97f9b1b33fe3ce951d7a33f7924e
Minh Tran <mctran98@yahoo.com>
2021-01-14 12:24:31 -0800

    Added useFindAndModify option

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/questions', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/questions', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 const db = mongoose.connection;
 
 module.exports = db;
